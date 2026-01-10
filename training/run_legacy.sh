7bb70fecf4e0c130e062d9057991a96f47d40757
Bishop Johnson <BishopJohnson@users.noreply.github.com>
2026-01-09 17:19:34 -0800

    Legacy run script (#15)
    - Added run_legacy.sh for original vosk run script
    - Changed data preparation step in run.sh to work for expected up-to-date data
    - Changed number of jobs of lookahead command in decode step to 10
    - Separated rescore from decode step into its own step

--- training/run_legacy.sh
+++ training/run_legacy.sh
@@ -1,18 +1,28 @@
-#!/usr/bin/env bash
+﻿#!/usr/bin/env bash
 
 . ./cmd.sh
 . ./path.sh
 
 stage=-1
 stop_stage=100
-dynamic_graph=false
 
 . utils/parse_options.sh
 
 # Data preparation
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
-  local/data_prep.sh corpus/audio/train-clean data/train
-  local/data_prep.sh corpus/audio/test-clean data/test
+  data_url=www.openslr.org/resources/31
+  lm_url=www.openslr.org/resources/11
+  database=corpus
+
+  mkdir -p $database
+  for part in dev-clean-2 train-clean-5; do
+    local/download_and_untar.sh $database $data_url $part
+  done
+
+  local/download_lm.sh $lm_url $database data/local/lm
+
+  local/data_prep.sh $database/LibriSpeech/train-clean-5 data/train
+  local/data_prep.sh $database/LibriSpeech/dev-clean-2 data/test
 fi
 
 # Dictionary formatting
@@ -64,16 +74,10 @@ fi
 # Decode
 if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${KALDI_ROOT}/tools/openfst/lib/fst
-
-  utils/format_lm.sh data/lang data/local/lm/lm.arpa.gz data/local/lm/vocab-full.txt data/lang_test
-
-  if [ ${dynamic_graph} = true ]; then
-    utils/mkgraph_lookahead.sh --self-loop-scale 1.0 data/lang_test exp/chain/tdnn exp/chain/tdnn/graph
-  else
-    utils/mkgraph.sh --self-loop-scale 1.0 data/lang_test exp/chain/tdnn exp/chain/tdnn/graph
-  fi
-
-  utils/build_const_arpa_lm.sh data/local/lm/lm.arpa.gz data/lang data/lang_test_rescore
+  
+  utils/format_lm.sh data/lang data/local/lm/lm_tgsmall.arpa.gz data/local/dict/lexicon.txt data/lang_test
+  utils/mkgraph.sh --self-loop-scale 1.0 data/lang_test exp/chain/tdnn exp/chain/tdnn/graph
+  utils/build_const_arpa_lm.sh data/local/lm/lm_tgmed.arpa.gz data/lang data/lang_test_rescore
 
   for task in test; do
     steps/make_mfcc.sh --cmd "$train_cmd" --nj 10 data/$task exp/make_mfcc/$task $mfcc
@@ -83,39 +87,20 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
         data/${task} exp/chain/extractor \
         exp/chain/ivectors_${task}
 
-    if [ ${dynamic_graph} = true ]; then
-      steps/nnet3/decode_lookahead.sh --cmd $decode_cmd \
-        --nj 10 \
-        --beam 13.0 \
-        --max-active 7000 \
-        --lattice-beam 4.0 \
-        --online-ivector-dir exp/chain/ivectors_${task} \
-        --acwt 1.0 \
-        --post-decode-acwt 10.0 \
-        --use-gpu true \
-        exp/chain/tdnn/graph data/${task} exp/chain/tdnn/decode_${task}
-      else
-        steps/nnet3/decode.sh --cmd $decode_cmd \
-              --num-threads 10 \
-              --nj 10 \
-              --beam 13.0 \
-              --max-active 7000 \
-              --lattice-beam 4.0 \
-              --online-ivector-dir exp/chain/ivectors_${task} \
-              --acwt 1.0 \
-              --post-decode-acwt 10.0 \
-              --use-gpu true \
-              exp/chain/tdnn/graph data/${task} exp/chain/tdnn/decode_${task}
-      fi
-  done
-fi
+    steps/nnet3/decode.sh --cmd $decode_cmd \
+      --num-threads 10 \
+      --nj 1 \
+      --beam 13.0 \
+      --max-active 7000 \
+      --lattice-beam 4.0 \
+      --online-ivector-dir exp/chain/ivectors_${task} \
+      --acwt 1.0 \
+      --post-decode-acwt 10.0 \
+      exp/chain/tdnn/graph data/${task} exp/chain/tdnn/decode_${task}
 
-# Test
-if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
-  for task in test; do
     steps/lmrescore_const_arpa.sh data/lang_test data/lang_test_rescore \
-            data/${task} exp/chain/tdnn/decode_${task} exp/chain/tdnn/decode_${task}_rescore
+        data/${task} exp/chain/tdnn/decode_${task} exp/chain/tdnn/decode_${task}_rescore
   done
-  
+
   bash RESULTS
 fi
